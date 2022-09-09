a75002ff24938082c4be8d4d31cb399ea6daf1f9
Alisa <petrunina.alisa@gmail.com>
2022-09-09 20:28:03 +0300

    task4

---
+++ task3.py
@@ -0,0 +1,10 @@
+# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+N = int(input('Введите число: '))
+list = []
+sum = 0 
+for i in range(1, N+1):
+    Ni = round((1+(1/i))**i,2)
+    list.append(Ni) 
+    sum += round(Ni,2)
+list.append(sum)
